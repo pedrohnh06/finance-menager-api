77d65a5e5360a2605290806e3d93d024175bafd1
Pedro Henrique Nascimento <pedro.hn.henrique.nascimento@gmail.com>
2026-08-04 18:31:16 -0300

    fix: adiciona CORS para liberar acesso do frontend

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from src.routers import categorias, transacoes, usuario, login
 from fastapi import FastAPI
 from src import models, database
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=[True],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def raiz():
