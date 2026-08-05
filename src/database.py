27c84a7c2d5d90039cd136f0d3c48ba8f2aae1c4
Pedro Henrique Nascimento <pedro.hn.henrique.nascimento@gmail.com>
2026-08-05 16:44:56 -0300

    refactor: migra banco de dados de SQLite para PostgreSQL

--- src/database.py
+++ src/database.py
@@ -10,9 +10,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./financeiro.db")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
